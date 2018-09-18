fa1a73226d6ea58e714f71dadf6f4b91fbaa0811
Hack Reactor Student <student@hackreactor.com>
2018-09-17 21:48:08 -0700

    submissions

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,35 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  //input: string, which represents className
+  //output: an array, containing all elements with input className
+  //strategy: check length of the children prop
+//if length > 0, check the class, then recurse
+//otherwise, just check the class and return our array
+
+  var result = [];
+
+  let obj;
+
+  if(arguments[1] === undefined) {
+    obj = document.body; // document.body;
+  } else {
+    obj = arguments[1];
+  };
+
+  if(obj.classList !== undefined) {
+    if (obj.classList.contains(className)) { // classList.contains(className)
+      result = result.concat(obj);
+    };
+  };  
+
+  if (obj.childNodes.length === 0) { // childNodes.length
+    return result;
+  } 
+
+  for(let i = 0; i < obj.childNodes.length; i++) {
+    result = result.concat(getElementsByClassName(className, obj.childNodes[i])); //forEach of ChildNodes
+  };
+
+  return result;
 };
