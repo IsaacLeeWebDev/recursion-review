9752e43f9036948efa73d518707170c0c42a5d11
Hack Reactor Student <student@hackreactor.com>
2018-09-17 21:50:10 -0700

    submissions

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,10 +6,10 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
-  //input: string, which represents className
-  //output: an array, containing all elements with input className
-  //strategy: check length of the children prop
+// your code here
+//input: string, which represents className
+//output: an array, containing all elements with input className
+//strategy: check length of the children prop
 //if length > 0, check the class, then recurse
 //otherwise, just check the class and return our array
 
@@ -17,25 +17,25 @@ var getElementsByClassName = function(className
 
   let obj;
 
-  if(arguments[1] === undefined) {
+  if (arguments[1] === undefined) {
     obj = document.body; // document.body;
   } else {
     obj = arguments[1];
-  };
+  }
 
-  if(obj.classList !== undefined) {
+  if (obj.classList !== undefined) {
     if (obj.classList.contains(className)) { // classList.contains(className)
       result = result.concat(obj);
-    };
-  };  
+    }
+  } 
 
   if (obj.childNodes.length === 0) { // childNodes.length
     return result;
-  } 
+  }
 
-  for(let i = 0; i < obj.childNodes.length; i++) {
+  for (let i = 0; i < obj.childNodes.length; i++) {
     result = result.concat(getElementsByClassName(className, obj.childNodes[i])); //forEach of ChildNodes
-  };
+  }
 
   return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -7,71 +7,69 @@ var stringifyJSON = function(obj) {
   
   if (Number.isNaN(obj) || obj === null) {
     return 'null';
-  }  
+  }
 
   if (typeof(obj) === 'number' || typeof(obj) === 'boolean' || Object.prototype.toString.call(obj) === '[object Date]') {
     return String(obj);
   }
 
-  if (typeof(obj) === undefined ||  obj === undefined || typeof(obj) === 'symbol' || typeof(obj) === 'function') {
+  if (typeof(obj) === undefined || obj === undefined || typeof(obj) === 'symbol' || typeof(obj) === 'function') {
     return undefined;
   }
 
   if (typeof(obj) === 'string') {
-    return String("\"" + obj + "\"");
+    return String('\"' + obj + '\"');
   }
-
   
   if (typeof(obj) === 'object') {
-    var result = "";
+    var result = '';
   
     if (Array.isArray(obj)) {
 
-      result = result.concat("[");
+      result = result.concat('[');
 
       for (var i = 0; i < obj.length; i++) {
         if (i === obj.length - 1) {
           result = result.concat(stringifyJSON(obj[i]));
         } else {
-          result = result.concat(stringifyJSON(obj[i]) + ",");
+          result = result.concat(stringifyJSON(obj[i]) + ',');
         }
       }
       
-      result = result.concat("]");
+      result = result.concat(']');
 
       return result;
     }
 
     for (var key in obj) {
       if (obj[key] !== undefined && stringifyJSON(obj[key]) !== undefined) {
-        result = result.concat(stringifyJSON(key) + ":" + stringifyJSON(obj[key]) + ",");
+        result = result.concat(stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',');
       }
     }
-    result = result.split("").slice(0, result.length - 1).join("");
-    result = "{" + result + "}";
+    result = result.split('').slice(0, result.length - 1).join('');
+    result = '{' + result + '}';
     return result;
   }
  
-  
 };
 
-  // your code goes here
-  //input: object
-  //output: a string comprised of: 
-  //for objects: property names get stringified
-  //for arrays: no property names or quotation marks around brackets
+// your code goes here
+//input: object
+//output: a string comprised of: 
+//for objects: property names get stringified
+//for arrays: no property names or quotation marks around brackets
 
-// > "{"x":5,"y":6}"
-// > "[3,"false",false]"
-// > "{"null":[10,null,null,null]}"
-// ""2006-01-02T23:04:05.000Z""
-// "numbers" "2" 
-// "strings" ""hi""
-// "booleans" "true"
-// "undefined" undefined
-// "null" "null"
-// "NaN" "null"
-// > "{}" "{}"
-// > "[]" "[]"
-// "Symbol("")" undefined
-// "functions" undefined
+// > '{'x':5,'y':6}'
+// > '[3,'false',false]'
+// > '{'null':[10,null,null,null]}'
+// ''2006-01-02T23:04:05.000Z''
+// 'numbers' '2' 
+// 'strings' ''hi''
+// 'booleans' 'true'
+// 'undefined' undefined
+// 'null' 'null'
+// 'NaN' 'null'
+// > '{}' '{}'
+// > '[]' '[]'
+// 'Symbol('')' undefined
+// 'functions' undefined
